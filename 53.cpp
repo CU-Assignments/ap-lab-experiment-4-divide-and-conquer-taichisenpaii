7d488847391ce891e3310148410cc42bbbf664ba
Vinayak garg <backpackervinayak@gmail.com>
2025-02-14 10:42:41 +0530

    Create 53.cpp
    53. Maximum Subarray

---
+++ 53.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+/*
+nput
+nums =
+[-2,1,-3,4,-1,2,1,-5,4]
+Output
+6
+Expected
+6
+*/
